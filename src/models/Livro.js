211c7d95df35ed13c078ff5c29600e270aea6a5f
Antonybsb <artbsb1@gmail.com>
2025-02-26 08:50:43 -0300

    Commit Initial

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -1,15 +1,33 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
-const livroSchema = new mongoose.Schema(
-  {
-    id: {type: String},
-    titulo: {type: String, required: true},
-    autor: {type: mongoose.Schema.Types.ObjectId, ref: 'autores', required: true},
-    editora: {type: String, required: true},
-    numeroPaginas: {type: Number}
+const livroSchema = new mongoose.Schema({
+  id: { type: String },
+  titulo: { type: String, required: [true, 'O título do livro é obrigatório'] },
+  autor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'autores',
+    required: [true, 'O autor(a) é obrigatório']
+  },
+  editora: {
+    type: String,
+    required: [true, 'O nome da editora é obrigatório'],
+    enum: {
+      values: ['Casa do código', 'Alura'],
+      message: 'A editora {VALUE} não é um valor permitido.'
+    }
+  },
+  numeroPaginas: {
+    type: Number,
+    validate: {
+      validator: valor => {
+        return valor >= 10 && valor <= 5000
+      },
+      message:
+        'O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}'
+    }
   }
-);
+})
 
-const livros= mongoose.model('livros', livroSchema);
+const livros = mongoose.model('livros', livroSchema)
 
-export default livros;
+export default livros
